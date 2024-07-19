2408e602e9fc567918d87d95c3e83d8662978597
bhaskar katara <bhaskarkatararjo5@gmail.com>
2024-07-19 12:50:14 +0530

    Add:AppIcon

--- app/src/main/java/com/example/weather_app_injetpackcomposeandroid/api/NetworkResponse.kt
+++ app/src/main/java/com/example/weather_app_injetpackcomposeandroid/api/NetworkResponse.kt
@@ -1,6 +1,6 @@
 package com.example.weather_app_injetpackcomposeandroid.api
 
-// T refers to WeatherModel
+// T refers to WeatherModel,generic class
 sealed class NetworkResponse<out  T>{
   data class Success<out T>(val data : T) : NetworkResponse<T>()
    data class Error(val message : String) : NetworkResponse<Nothing>()
