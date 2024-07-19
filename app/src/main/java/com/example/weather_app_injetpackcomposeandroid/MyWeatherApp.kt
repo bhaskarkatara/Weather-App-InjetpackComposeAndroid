b2fd9c158be853044c2440c7da596117b729b2db
bhaskar katara <bhaskarkatararjo5@gmail.com>
2024-07-19 12:12:32 +0530

    Add:network Response handle class

--- app/src/main/java/com/example/weather_app_injetpackcomposeandroid/MyWeatherApp.kt
+++ app/src/main/java/com/example/weather_app_injetpackcomposeandroid/MyWeatherApp.kt
@@ -6,29 +6,40 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.weather_app_injetpackcomposeandroid.api.NetworkResponse
 
 @Composable
 fun MyWeatherApp(modifier: Modifier,viewModel: WeatherViewModel) {
     var City by remember { mutableStateOf("") }
-    Column(modifier = Modifier.fillMaxSize().padding(30.dp),
+    val weatherResult = viewModel.weatherResult.observeAsState()
+
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(30.dp),
      horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Row(
-            modifier = Modifier.fillMaxWidth().padding(10.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
@@ -42,5 +53,18 @@ fun MyWeatherApp(modifier: Modifier,viewModel: WeatherViewModel) {
                 Icon(imageVector = Icons.Default.Search, contentDescription = null)
             }
         }
+        when(val result = weatherResult.value){
+            is NetworkResponse.Success -> {
+                Text(text = result.data.toString())
+            }
+            is NetworkResponse.Error -> {
+                Text(text = result.message)
+            }
+            is NetworkResponse.Loading -> {
+                CircularProgressIndicator(modifier = Modifier.wrapContentSize(Alignment.Center))
+            }
+            else -> {}
+        }
     }
+
 }
--- app/src/main/java/com/example/weather_app_injetpackcomposeandroid/WeatherViewModel.kt
+++ app/src/main/java/com/example/weather_app_injetpackcomposeandroid/WeatherViewModel.kt
@@ -2,23 +2,38 @@ package com.example.weather_app_injetpackcomposeandroid
 
 import android.content.ContentValues.TAG
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.weather_app_injetpackcomposeandroid.api.Constant
+import com.example.weather_app_injetpackcomposeandroid.api.NetworkResponse
 import com.example.weather_app_injetpackcomposeandroid.api.RetrofitInstance
+import com.example.weather_app_injetpackcomposeandroid.api.WeatherModel
 import kotlinx.coroutines.launch
 
 class WeatherViewModel :ViewModel() {
 
     private val weatherApi = RetrofitInstance.weatherApi
+    private val _weatherResult = MutableLiveData<NetworkResponse<WeatherModel>>()
+     val weatherResult : LiveData<NetworkResponse<WeatherModel>> = _weatherResult
+
      fun getData(City:String){
+         _weatherResult.value = NetworkResponse.Loading
          viewModelScope.launch {
-            val resposne =  weatherApi.getWeatherData(Constant.apikey,City)
-             if(resposne.isSuccessful){
-                 Log.i(TAG, "getData: ${resposne.body().toString()}")
-             }else{
-             Log.i(TAG, "getData: ${resposne.message()}")
-             }
+            try {
+                val response =  weatherApi.getWeatherData(Constant.apikey,City)
+                if(response.isSuccessful){
+                    response.body()?.let{
+                        _weatherResult.value = NetworkResponse.Success(it)
+                    }
+                }else{
+                    _weatherResult.value = NetworkResponse.Error("Failed to load data")
+                }
+            }
+            catch (e:Exception){
+                _weatherResult.value = NetworkResponse.Error("Failed to load data")
+            }
          }
 
     }
